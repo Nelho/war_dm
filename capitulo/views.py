acbddf9c187363658ffd5dd110cd5d5f8ca8b864
oguiraw <sergio.ewerton@dce.ufpb.br>
2017-09-27 02:53:23 -0300

    Cadastro de capíto em 80%

--- capitulo/views.py
+++ capitulo/views.py
@@ -7,17 +7,24 @@ def cadastrarCapitulo(request):
     if(request.method == 'POST'):
         form = CapituloUserForm(request.POST)
         if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = User(username=username, password=password)
+            user.save()
+
+            regiao = form.cleaned_data['regiao']
+            usuario = Usuario(user=user, regiao=regiao, tipoUsuario='CAP')
+            usuario.save()
+
             avaliador = form.cleaned_data['avaliador']
-            userCapitulo = form.cleaned_data['usuarioCap']
             dataFundacao = form.cleaned_data['dataFundacao']
             dataInstacao = form.cleaned_data['dataInstalacao']
             mestreConsenheiro = form.cleaned_data['mestreCosenheiro']
             numero = form.cleaned_data['numero']
-
-            capitulo = UsuarioCapitulo(avaliador = avaliador, usuarioCap= userCapitulo, mestreConselheiro= mestreConsenheiro,
-                                       dataFundacao= dataFundacao, dataInstalacao= dataInstacao, numero= numero)
-            capitulo.save()
+            usuarioCap = UsuarioCapitulo(numero=numero, dataFundacao=dataFundacao, dataInstalacao=dataInstacao,
+                                         mestreConselheiro=mestreConsenheiro, avaliador=avaliador, usuarioCap=usuario)
+            usuarioCap.save()
         return HttpResponseRedirect('/admin')
     form = CapituloUserForm()
-    context_form_set = {"form" : form.as_p()}
-    return render(request, 'capitulo/cadastroCapitulo.html', context=context_form_set)
+    context = {"form" : form}
+    return render(request, 'capitulo/cadastroCapitulo.html', context=context)
