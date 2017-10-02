d6e2b4dbe45c8bd4802afa4fbe95d80fa48a64c0
Neto Breba <jose.silvaneto@dce.ufpb.br>
2017-10-02 02:01:25 -0300

    integrando widget_tweaks a tela de cadastro de avaliador

--- avaliador/views.py
+++ avaliador/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from main.models import Usuario
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
+from main.models import Contato
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -11,10 +13,13 @@ def cadastroAvaliador(request):
 	if request.method == "POST":
 		form = AvaliadorForm(request.POST, request.FILES)
 		if form.is_valid():
-			form_nome = form.cleaned_data["nome"]
-			form_login = form.cleaned_data["login"]
 			form_senha = form.cleaned_data["senha"]
 			form_confirmar_senha = form.cleaned_data["confirmar_senha"]
+			if form_senha != form_confirmar_senha:
+				context = {"form": form}
+				return render(request, "avaliador/cadastro_avaliador.html", context=context)
+			form_nome = form.cleaned_data["nome"]
+			form_login = form.cleaned_data["login"]
 			form_regiao = form.cleaned_data["regiao"]
 			form_email = form.cleaned_data["email"]
 			form_telefone = form.cleaned_data["telefone"]
@@ -23,16 +28,17 @@ def cadastroAvaliador(request):
 			#form_foto.name = form_login + form_foto.name[form_foto.name.find("."):]
 
 			new_user = User.objects.create_user(form_login, password=form_senha, 
-				first_name=form_nome,
-				last_name=form_sobrenome)
+				first_name=form_nome, email=form_email)
 			new_user.save()
 			new_usuario = Usuario(user=new_user, 
 				regiao=form_regiao,
 				tipoUsuario=TIPO_USUARIO,
 				foto=form_foto)
-
 			new_usuario.save()
-			return HttpResponse("pegou")
+			new_contato = Contato(usuario=new_user, contato=form_telefone)
+			new_contato.save()
+
+			return HttpResponseRedirect('/avaliador/cadastro')
 
 	form = AvaliadorForm()
 	context = {"form": form}
