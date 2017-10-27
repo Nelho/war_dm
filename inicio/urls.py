af81426d8404aaa5c74bd108f0b9b258d1777915
Uélio Dornelas <uelio.dornelas@dce.ufpb.br>
2017-10-27 16:17:47 -0200

    Linkando todos os templates

--- inicio/urls.py
+++ inicio/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+
     url(r'^mapa/', views.mapa_inicio, name='mapa_inicio'),
     url(r'^regras/', views.regras_inicio, name='regras_inicio'),
-    url(r'^avaliadores/', views.avaliadores, name='avaliadores')
+    url(r'^avaliadores/', views.avaliadores, name='avaliadores'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
+
+
 def mapa_inicio(request):
     return render(request, 'inicio/mapa.html')
 
