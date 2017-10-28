ee50d70d043060b641de9957f1c25024e0d7ba4f
José Antonio <jose.silvaneto@dce.ufpb.br>
2017-10-27 21:42:56 -0300

    criação do mapa geral no template início

--- inicio/views.py
+++ inicio/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from avaliador.models import Gabinete_User
+from mapa.views import mapa_geral as mapa_geral_home
 
 
 def mapa_inicio(request):
-    return render(request, 'inicio/mapa.html')
+    conquistas = mapa_geral_home()
+    context = {"conquistas": conquistas}
+    return render(request, "inicio/mapa.html", context=context)
 
 def regras_inicio(request):
     return render(request, 'inicio/regras.html')
@@ -12,3 +15,4 @@ def avaliadores(request):
 	avaliadores = Gabinete_User.objects.all()
 	context_dict = {'avaliadores': avaliadores}
 	return render(request, 'inicio/avaliadores.html', context = context_dict)
+
